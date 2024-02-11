42e51731dd884b100c597b03c7560b65e559eff7
Sall <sallaboudaouda@gmail.com>
2024-02-11 19:37:46 +0000

    add showcase

---
+++ src/components/ui/showcase.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../layout/header";
+
+export default function Showcase() {
+  return (
+    <div className="h-_784 bg-hero-pattern bg-contain">
+      <Header />
+    </div>
+  );
+}
