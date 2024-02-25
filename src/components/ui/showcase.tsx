9beeeef183b9c5812d5d883c4b7a1e517f2d399e
Sall <sallaboudaouda@gmail.com>
2024-02-25 12:06:41 +0000

    added some animation

--- src/components/ui/showcase.tsx
+++ src/components/ui/showcase.tsx
@@ -3,81 +3,126 @@ import React from "react";
 import Header from "../layout/header";
 import { Button } from ".";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function Showcase() {
   return (
     <div className="h-_784 bg-hero-pattern bg-contain">
       <Header />
       <div className="flex flex-col px-12 lg:flex-row lg:px-0">
-        <div className="mt-16 font-bold text-white lg:pl-_108">
-          <h2 className="text-2xl">Furniture</h2>
-          <div className="mt-8 flex flex-col items-center gap-8 md:w-96 lg:w-_200">
-            <p className="flex flex-col gap-1 text-xs">
-              <span>
-                Lorem ipsum dolor sit amet consectetur. At rhoncus etiam
-                convallis ullamcorper sit. Velit pretium vitae molestie viverra
-                nibh posuere.
-              </span>
-              <span>
-                Lorem ipsum dolor sit amet consectetur. At rhoncus etiam
-                convallis ullamcorper sit. Velit pretium vitae molestie viverra
-                nibh posuere.
-              </span>
-            </p>
-            <Button
-              type="button"
-              onClick={() => {}}
-              label="read more"
-              variant="primary"
-              size="sm"
-            />
+        <motion.div
+          className="mt-16 font-bold text-white lg:pl-_108"
+          initial={{ x: -1000 }}
+          animate={{ x: 0 }}
+          transition={{
+            type: "spring",
+            stiffness: 50,
+            duration: 0.8,
+            delay: 0.5,
+          }}
+        >
+          <div className="mt-16 font-bold text-white">
+            <h2 className="text-2xl">Furniture</h2>
+            <div className="mt-8 flex flex-col items-center gap-8 md:w-96 lg:w-_200">
+              <p className="flex flex-col gap-1 text-xs">
+                <span>
+                  Lorem ipsum dolor sit amet consectetur. At rhoncus etiam
+                  convallis ullamcorper sit. Velit pretium vitae molestie
+                  viverra nibh posuere.
+                </span>
+                <span>
+                  Lorem ipsum dolor sit amet consectetur. At rhoncus etiam
+                  convallis ullamcorper sit. Velit pretium vitae molestie
+                  viverra nibh posuere.
+                </span>
+              </p>
+              <Button
+                type="button"
+                onClick={() => {}}
+                label="read more"
+                variant="primary"
+                size="sm"
+              />
+            </div>
           </div>
-        </div>
+        </motion.div>
 
         <div className="mt-28 text-center text-white md:mt-8 lg:ml-10 lg:mt-32 lg:w-_469">
           <div className="relative">
-            <h2 className="font-border text-5xl font-semibold capitalize leading-tight text-white md:text-64 md:leading-normal">
-              Make a room comfortable & elegant
-            </h2>
-            <Image
-              src="/images/Highlight.png"
-              alt="hero"
-              width={39}
-              height={43}
+            <motion.div
+              className="mt-8"
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
+            >
+              <h2 className="font-border text-5xl font-semibold capitalize leading-tight text-white md:text-64 md:leading-normal">
+                Make a room comfortable & elegant
+              </h2>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.9,
+                delay: 1.5,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
               className="absolute -top-[15px] left-6"
-            />
+            >
+              <Image
+                src="/images/Highlight.png"
+                alt="hero"
+                width={39}
+                height={43}
+              />
+            </motion.div>
           </div>
         </div>
+        <motion.div
+          className="mt-8"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
+          <div className="ml-56 mt-_88 hidden text-white desktop:block">
+            <h2 className="text-2xl font-bold">Featured</h2>
+            <div className="mt-7 flex flex-col gap-16">
+              <div className="relative">
+                <Image
+                  src="/images/hero-1.png"
+                  alt="hero"
+                  width={170}
+                  height={166}
+                />
+                <p className="featured-pricing absolute flex flex-col capitalize text-black">
+                  <span>Blue sofa</span>
+                  <span>$250</span>
+                </p>
+              </div>
 
-        <div className="ml-56 mt-_88 hidden text-white desktop:block">
-          <h2 className="text-2xl font-bold">Featured</h2>
-          <div className="mt-7 flex flex-col gap-16">
-            <div className="relative">
-              <Image
-                src="/images/hero-1.png"
-                alt="hero"
-                width={170}
-                height={166}
-              />
-              <p className="featured-pricing absolute flex flex-col capitalize text-black">
-                <span>Blue sofa</span>
-                <span>$250</span>
-              </p>
-            </div>
-            <div className="relative">
-              <Image
-                src="/images/hero-2.png"
-                alt="hero"
-                width={170}
-                height={166}
-              />
-              <p className="featured-pricing absolute flex flex-col capitalize text-black">
-                <span>Orange chair</span>
-                <span>$250</span>
-              </p>
+              <div className="relative">
+                <Image
+                  src="/images/hero-2.png"
+                  alt="hero"
+                  width={170}
+                  height={166}
+                />
+                <p className="featured-pricing absolute flex flex-col capitalize text-black">
+                  <span>Orange chair</span>
+                  <span>$250</span>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
--- src/components/ui/why-choosing-us.tsx
+++ src/components/ui/why-choosing-us.tsx
@@ -1,114 +1,149 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from ".";
 import Image from "next/image";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 export default function WhyChoosingUs() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
   return (
     <div className="mt-20 flex flex-col items-center justify-center gap-8 px-12 md:mt-40 md:flex-row lg:gap-16 lg:px-28 desktop:gap-_196">
-      <div className="md:w-_448">
-        <h2 className="mb-5 inline-flex flex-col text-40 font-semibold">
-          <span>Why choosing</span> <span className="text-center">us</span>
-        </h2>
-        <p className="mb-5 flex flex-col gap-4 text-13">
-          <span>
-            Lorem ipsum dolor sit amet consectetur. Molestie vel nisl justo
-            vitae ridiculus malesuada id dapibus posuere. At in ac duis tortor
-            sem lectus eget. Purus arcu massa eget massa risus fringilla. Lacus
-            condimentum pulvinar vulputate natoque. Ac consequat eget fusce
-            nulla viverra. Nulla nibh massa mattis in. Tristique a tellus
-            placerat ac eget. Enim nunc augue fringilla amet. Parturient ornare
-            blandit fames auctor augue.e. Ac consequat eget fusce nulla viverra.
-          </span>
-          <span>
-            Nulla nibh massa mattis in. Tristique a tellus placerat ac eget.
-            Enim nunc augue fringilla amet. Parturient ornare blandit fames
-            auctor augue.e. Ac consequat eget fusce nulla viverra. Nulla nibh
-            massa mattis in. Tristique a tellus placerat ac eget. Enim nunc
-            augue fringilla amet. Parturient ornare blandit fames auctor augue.
-          </span>
+      <motion.div
+        ref={ref}
+        animate={controls}
+        initial="hidden"
+        transition={{ duration: 0.3 }}
+        variants={{
+          visible: { opacity: 1, scale: 1 },
+          hidden: { opacity: 0, scale: 0 },
+        }}
+      >
+        <div className="md:w-_448">
+          <h2 className="mb-5 inline-flex flex-col text-40 font-semibold">
+            <span>Why choosing</span> <span className="text-center">us</span>
+          </h2>
+          <p className="mb-5 flex flex-col gap-4 text-13">
+            <span>
+              Lorem ipsum dolor sit amet consectetur. Molestie vel nisl justo
+              vitae ridiculus malesuada id dapibus posuere. At in ac duis tortor
+              sem lectus eget. Purus arcu massa eget massa risus fringilla.
+              Lacus condimentum pulvinar vulputate natoque. Ac consequat eget
+              fusce nulla viverra. Nulla nibh massa mattis in. Tristique a
+              tellus placerat ac eget. Enim nunc augue fringilla amet.
+              Parturient ornare blandit fames auctor augue.e. Ac consequat eget
+              fusce nulla viverra.
+            </span>
+            <span>
+              Nulla nibh massa mattis in. Tristique a tellus placerat ac eget.
+              Enim nunc augue fringilla amet. Parturient ornare blandit fames
+              auctor augue.e. Ac consequat eget fusce nulla viverra. Nulla nibh
+              massa mattis in. Tristique a tellus placerat ac eget. Enim nunc
+              augue fringilla amet. Parturient ornare blandit fames auctor
+              augue.
+            </span>
 
-          <span>
-            e. Ac consequat eget fusce nulla viverra. Nulla nibh massa mattis
-            in. Tristique a tellus placerat ac eget. Enim nunc augue fringilla
-            amet. Parturient ornare blandit fames auctor augue.
-          </span>
-        </p>
-        <Button
-          variant="primary"
-          label="Read more"
-          size="sm"
-          onClick={() => {}}
-        />
-      </div>
-      <div className="why-choosing-us  flex flex-col gap-9 md:flex-row">
-        <div className="why-choosing-card-container">
-          <div className="why-choosing-card bg-primary-300">
-            <div className="why-choosing-card-round">
+            <span>
+              e. Ac consequat eget fusce nulla viverra. Nulla nibh massa mattis
+              in. Tristique a tellus placerat ac eget. Enim nunc augue fringilla
+              amet. Parturient ornare blandit fames auctor augue.
+            </span>
+          </p>
+          <Button
+            variant="primary"
+            label="Read more"
+            size="sm"
+            onClick={() => {}}
+          />
+        </div>
+      </motion.div>
+      <motion.div
+        className="mt-16 font-bold text-white lg:pl-_108"
+        ref={ref}
+        animate={controls}
+        initial="hidden"
+        transition={{ duration: 0.3 }}
+        variants={{
+          visible: { opacity: 1, scale: 1 },
+          hidden: { opacity: 0, scale: 0 },
+        }}
+      >
+        <div className="why-choosing-us  flex flex-col gap-9 md:flex-row">
+          <div className="why-choosing-card-container">
+            <div className="why-choosing-card bg-primary-300">
+              <div className="why-choosing-card-round">
+                <Image
+                  src="/images/delivery.png"
+                  alt="hero"
+                  width={58}
+                  height={58}
+                />
+              </div>
+              <h2>
+                free <br /> Delivery
+              </h2>
+            </div>
+            <div className="why-choosing-card relative bg-secondary-100">
+              {" "}
+              <div className="why-choosing-card-round">
+                <Image
+                  src="/images/wrench.png"
+                  alt="hero"
+                  width={58}
+                  height={58}
+                />
+              </div>
+              <h2>
+                free <br /> installation
+              </h2>
               <Image
-                src="/images/delivery.png"
+                src="/images/ellipse-sm-1.png"
                 alt="hero"
-                width={58}
-                height={58}
+                width={61}
+                height={119}
+                className="absolute bottom-7 left-0"
               />
             </div>
-            <h2>
-              free <br /> Delivery
-            </h2>
           </div>
-          <div className="why-choosing-card relative bg-secondary-100">
-            {" "}
-            <div className="why-choosing-card-round">
+          <div className="why-choosing-card-container mt-16">
+            <div className="why-choosing-card relative bg-secondary-100">
+              {" "}
+              <div className="why-choosing-card-round">
+                <Image src="/images/ok.png" alt="hero" width={58} height={58} />
+              </div>
+              <h2>
+                secure <br /> payment
+              </h2>
               <Image
-                src="/images/wrench.png"
+                src="/images/ellipse-sm-2.png"
                 alt="hero"
-                width={58}
-                height={58}
+                width={61}
+                height={119}
+                className="absolute bottom-7 right-0"
               />
             </div>
-            <h2>
-              free <br /> installation
-            </h2>
-            <Image
-              src="/images/ellipse-sm-1.png"
-              alt="hero"
-              width={61}
-              height={119}
-              className="absolute bottom-7 left-0"
-            />
+            <div className="why-choosing-card bg-primary-300">
+              {" "}
+              <div className="why-choosing-card-round">
+                <Image
+                  src="/images/square.png"
+                  alt="hero"
+                  width={58}
+                  height={58}
+                />
+              </div>
+              <h2>Guarantee</h2>
+            </div>
           </div>
         </div>
-        <div className="why-choosing-card-container mt-16">
-          <div className="why-choosing-card relative bg-secondary-100">
-            {" "}
-            <div className="why-choosing-card-round">
-              <Image src="/images/ok.png" alt="hero" width={58} height={58} />
-            </div>
-            <h2>
-              secure <br /> payment
-            </h2>
-            <Image
-              src="/images/ellipse-sm-2.png"
-              alt="hero"
-              width={61}
-              height={119}
-              className="absolute bottom-7 right-0"
-            />
-          </div>
-          <div className="why-choosing-card bg-primary-300">
-            {" "}
-            <div className="why-choosing-card-round">
-              <Image
-                src="/images/square.png"
-                alt="hero"
-                width={58}
-                height={58}
-              />
-            </div>
-            <h2>Guarantee</h2>
-          </div>
-        </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
